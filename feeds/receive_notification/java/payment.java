b75aeaeeb2f702d2043488ca719eb3f460b47d6c
nrubio <nahuelr92@gmail.com>
2015-03-16 16:01:24 -0300

    two payment step notifications

--- feeds/receive_notification/java/payment.java
+++ feeds/receive_notification/java/payment.java
@@ -2,23 +2,42 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import mercadopago.MP;
+import com.mercadopago.MP;
 
 import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 
 public class ReceiveFeed extends HttpServlet {
  
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, JSONException, Exception {
 		MP mp = new MP ("CLIENT_ID", "CLIENT_SECRET");
 
-		JSONObject paymentInfo = mp.getPaymentInfo(request.getParameter("id"));
-
-		if (Integer.parseInt(paymentInfo.get("status").toString()) == 200) {
-			out.print(paymentInfo.get("response"));
+		JSONObject merchantOrderInfo=null;
+		JSONObject paymentInfo = null;
+		
+		if(request.getParameter("topic").equals("merchant_order")){
+			merchantOrderInfo = mp.getMerchantOrderInfo(request.getParameter("id"));
+		}else if(request.getParameter("topic").equals("payment")){
+			paymentInfo = mp.getPaymentInfo(request.getParameter("id"));
+			merchantOrderInfo = mp.getMerchantOrderInfo(paymentInfo.getJSONObject("response").getJSONObject("collection").getString("merchant_order_id"));
 		}
-	}
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
+		if (merchantOrderInfo.getInt("status") == 200) {
+			Double transactionAmountPayments = 0D;
+			Double transactionAmountOrder = merchantOrderInfo.getJSONObject("response").getDouble("total_amount");
+			JSONArray payments=merchantOrderInfo.getJSONObject("response").getJSONArray("payments");
+		    for (int i=0; i < payments.length(); i++) {
+		    	JSONObject payment = payments.getJSONObject(i);
+		    	if(payment.getString("status").equals("approved")){
+		    		transactionAmountPayments += payment.getDouble("transaction_amount");
+			    }	
+		    }
+		    if(transactionAmountPayments >= transactionAmountOrder){
+		    	System.out.print("release your items");
+		    }
+		}
+		
+		System.out.print("dont release your items");
 	}
 }
--- feeds/receive_notification/php/payment.php
+++ feeds/receive_notification/php/payment.php
@@ -3,9 +3,26 @@ require_once "mercadopago.php";
 
 $mp = new MP("CLIENT_ID", "CLIENT_SECRET");
 
-$payment_info = $mp->get_payment_info($_GET["id"]);
-
-if ($payment_info["status"] == 200) {
-	print_r($payment_info["response"]);
+if($_GET["topic"] == 'payment'){
+	$payment_info = $mp->get_payment_info($_GET["id"]);
+	$merchant_order_info = $mp->get_merchant_order_info($payment_info["response"]["collection"]["merchant_order_id"]);
+}else if($_GET["topic"] == 'merchant_order'){
+	$merchant_order_info = $mp->get_merchant_order_info($_GET["id"]);
 }
+
+if ($merchant_order_info["status"] == 200) {
+			$transaction_amount_payments= 0;
+			$transaction_amount_order = $merchant_order_info["response"]["total_amount"];
+		    $payments=$merchant_order_info["response"]["payments"];
+		    foreach ($payments as  $payment) {
+		    	if($payment['status'] == 'approved'){
+			    	$transaction_amount_payments += $payment['transaction_amount'];
+			    }	
+		    }
+		    if($transaction_amount_payments >= $transaction_amount_order){
+		    	echo "release your items";
+		    }
+	}
+echo "dont release your items";
+
 ?>
